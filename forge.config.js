08353583c29758bd8177e25045a9899e70b59ad5
Damian <damiansuaz@gmail.com>
2025-11-07 00:59:01 -0500

    Update for Windows and exports to other Linux formats

--- forge.config.js
+++ forge.config.js
@@ -6,6 +6,21 @@ module.exports = {
   makers: [
     {
       name: "@electron-forge/maker-zip"
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
+      config: {
+        name: "Shadowcast"
+      }
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      platforms: ["linux"]
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      platforms: ["linux"]
     }
   ]
 }
